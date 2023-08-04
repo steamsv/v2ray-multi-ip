52b253fd92da67b4d9c5084ee944d3bcab042406
steamsv <49923565+steamsv@users.noreply.github.com>
2023-08-04 23:25:42 +0800

    Update json.sh

--- json.sh
+++ json.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
-
-# 安装V2ray
-if systemctl status v2ray &> /dev/null; then
+# 检查 V2ray 是否已经安装
+if command -v v2ray >/dev/null 2>&1; then
     echo "V2ray已经安装"
 else
     echo "V2ray未安装，将进行安装"
-    bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
-
-
-if hash jq &> /dev/null; then
+    curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh | bash
+fi
+# 检查 jq 是否已经安装
+if hash jq 2>/dev/null; then
     echo "jq已经安装"
 else
     echo "jq未安装，将进行安装"
-    yum install jq -y
+    if ! yum install jq -y; then
+        echo "安装jq失败" >&2
+        exit 1
+    fi
 fi
-
-rm -rf /usr/local/etc/v2ray/config.json
-curl -L https://raw.githubusercontent.com/steamsv/ssduo/main/newconfig.json -o /usr/local/etc/v2ray/config.json
-# 获 取 服 务 器  IP 地 址 列 表
-ip_list=$(ip addr show|grep inet|grep -v 127.0.0.1|grep -v inet6|sed 's#/.*$##g'|awk '{print $2}'|tr -d "addr:")
-
-# 遍 历  IP 地 址 列 表 ， 为 每 个  IP 地 址 添 加 出 站 路 由
+# 删除旧的配置文件
+rm -f /usr/local/etc/v2ray/config.json
+# 下载新的配置文件
+if ! curl -L https://raw.githubusercontent.com/steamsv/ssduo/main/newconfig.json -o /usr/local/etc/v2ray/config.json; then
+    echo "下载新的配置文件失败" >&2
+    exit 1
+fi
+# 获取服务器IP地址列表
+ip_list=$(ip addr show | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}')
+# 为每个IP地址添加出站路由
 for ip in $ip_list; do
-    i=$((i+1))
-    tag="ip"$i""
+    i=1
+    tag="ip_$i"
     uuid=$(uuidgen)
-    user="user"$i"@v2ray.com"
+    user="user_$i@v2ray.com"
     
-    data=$(cat /usr/local/etc/v2ray/config.json)
-    
-    newdata=$(jq -r --arg ip "$ip" --arg tag "$tag" --arg uuid "$uuid" --arg user "$user" '.inbound.settings.clients += [{"id":$uuid,"alterId":0,"email":$user}] | .outboundDetour += [{"sendThrough":$ip,"protocol":"freedom","tag":$tag}] | .routing.rules += [{"type":"field","user":[$user],"outboundTag":$tag}]' <<< "$data")
-    echo "$newdata" > /usr/local/etc/v2ray/config.json
+    data=$(cat /usr/local/etc/v2ray/config.json) || exit 1 # 检查JSON语法是否正确
+    newdata=$(jq --arg ip "$ip" --arg tag "$tag" --arg uuid "$uuid" --arg user "$user" '.inbound.settings.clients += [{"id":$uuid,"alterId":0,"email":$user}] | .outboundDetour += [{"sendThrough":$ip,"protocol":"freedom","tag":$tag}] | .routing.rules += [{"type":"field","user":[$user],"outboundTag":$tag}] | .schemaVersion = "3"' <<< "$data") || exit 1 # 检查JSON语法是否正确
+    echo "$newdata" > /usr/local/etc/v2ray/config.json || exit 1 # 检查写入是否成功
 done
