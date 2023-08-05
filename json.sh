710e90b85dffe2ede3a4b1aefa836bbba974ccbc
steamsv <49923565+steamsv@users.noreply.github.com>
2023-08-05 22:31:56 +0800

    Update json.sh

--- json.sh
+++ json.sh
@@ -35,4 +35,9 @@ for ip in $ip_list; do
     data=$(cat /usr/local/etc/v2ray/config.json) || exit 1 # 检查JSON语法是否正确
     newdata=$(jq --arg ip "$ip" --arg tag "$tag" --arg uuid "$uuid" --arg user "$user" '.inbound.settings.clients += [{"id":$uuid,"alterId":0,"email":$user}] | .outboundDetour += [{"sendThrough":$ip,"protocol":"freedom","tag":$tag}] | .routing.rules += [{"type":"field","user":[$user],"outboundTag":$tag}]' <<< "$data") || exit 1 # 检查JSON语法是否正确
     echo "$newdata" > /usr/local/etc/v2ray/config.json || exit 1 # 检查写入是否成功
+    string="{\"add\":\"$ip\",\"aid\":\"0\",\"alpn\":\"\",\"fp\":\"\",\"host\":\"\",\"id\":\"$uuid\",\"net\":\"ws\",\"path\":\"ws\",\"port\":\"10400\",\"ps\":\"$ip\",\"scy\":\"auto\",\"sni\":\"\",\"tls\":\"\",\"type\":\"\",\"v\":\"2\"}"
+    encoded_string=$(echo -n "$string" | base64)
+    result="vmess://$encoded_string"
+    result=$(echo -n "$result" | tr -d '\n')
+echo "$result"
 done
