cd9d5eb09e3a96dd8f84eb0b06b7b127209c4f85
steamsv <49923565+steamsv@users.noreply.github.com>
2023-08-06 14:01:49 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -36,8 +36,6 @@ for ip in $ip_list; do
     data=$(cat /usr/local/etc/v2ray/config.json) || exit 1 # 检查JSON语法是否正确
     newdata=$(jq --arg ip "$ip" --arg tag "$tag" --arg uuid "$uuid" --arg user "$user" '.inbound.settings.clients += [{"email":"$user","method":"aes-256-gcm","password":"hvfdghvufgv","level":0,"network":"tcp,udp"}] | .outboundDetour += [{"sendThrough":$ip,"protocol":"freedom","tag":$tag}] | .routing.rules += [{"type":"field","user":[$user],"outboundTag":$tag}]' <<< "$data") || exit 1 # 检查JSON语法是否正确
     echo "$newdata" > /usr/local/etc/v2ray/config.json || exit 1 # 检查写入是否成功
-    string="{\"add\":\"$ip\",\"aid\":\"0\",\"alpn\":\"\",\"fp\":\"\",\"host\":\"\",\"id\":\"$uuid\",\"net\":\"ws\",\"path\":\"ws\",\"port\":\"8090\",\"ps\":\"$ip\",\"scy\":\"auto\",\"sni\":\"\",\"tls\":\"\",\"type\":\"\",\"v\":\"2\"}"
     echo "ss://YWVzLTI1Ni1nY206aHZmZGdodnVmZ3Y=@${ip}:8090#ss"
-    echo "$result"
 done
 systemctl restart v2ray
