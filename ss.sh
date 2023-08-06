d4a69590862053ca7d524eebc33a86cbea01a02c
steamsv <49923565+steamsv@users.noreply.github.com>
2023-08-06 14:38:30 +0800

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -24,6 +24,9 @@ if ! curl -L https://raw.githubusercontent.com/steamsv/v2ray-multi-ip/main/sscon
     echo "下载新的配置文件失败" >&2
     exit 1
 fi
+
+# 定义初始端口
+i=10000
 # 获取服务器IP地址列表
 ip_list=$(ip addr show|grep inet|grep -v 127.0.0.1|grep -v inet6|sed 's#/.*$##g'|awk '{print $2}'|tr -d "addr:")
 # 为每个IP地址添加出站路由
@@ -34,7 +37,7 @@ for ip in $ip_list; do
     user="user"$i"@v2ray.com"
     
     data=$(cat /usr/local/etc/v2ray/config.json) || exit 1 # 检查JSON语法是否正确
-    newdata=$(jq --arg ip "$ip" --arg tag "$tag" --arg uuid "$uuid" --arg user "$user" '.inbound.settings.clients += [{"email":$user,"method":"aes-256-gcm","password":"hvfdghvufgv","level":0,"network":"tcp,udp"}] | .outboundDetour += [{"sendThrough":$ip,"protocol":"freedom","tag":$tag}] | .routing.rules += [{"type":"field","user":[$user],"outboundTag":$tag}]' <<< "$data") || exit 1 # 检查JSON语法是否正确
+    newdata=$(jq --arg ip "$ip" --arg tag "$tag" --arg uuid "$uuid" --arg user "$user" '.inbound.settings.clients += [{"listen":"0.0.0.0","port":$i,"protocol":"shadowsocks","settings":{"clients":[{"email":$user,"method":"aes-256-gcm","password":"hvfdghvufgv","level":0,"network":"tcp,udp"}]}}] | .outboundDetour += [{"sendThrough":$ip,"protocol":"freedom","tag":$tag}] | .routing.rules += [{"type":"field","user":[$user],"outboundTag":$tag}]' <<< "$data") || exit 1 # 检查JSON语法是否正确
     echo "$newdata" > /usr/local/etc/v2ray/config.json || exit 1 # 检查写入是否成功
     echo "ss://YWVzLTI1Ni1nY206aHZmZGdodnVmZ3Y=@${ip}:8090#ss"
 done
